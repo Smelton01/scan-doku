9e9fbe0a72037286edb92ea06fa30b6bf1274516
SimonJuba <scimail09@gmail.com>
2021-10-24 23:21:31 +0900

    simple animations

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,82 +4,11 @@ import MyGrid from "./components/MyGrid";
 import { Container, Row, Col } from 'react-flexybox';
 import { Button } from '@material-ui/core';
 
-// export default class App extends Component {
-//   constructor(props){
-//     super(props);
-//     this.animateSolution = this.animateSolution.bind(this);
-//     this.anime = this.anime.bind(this);
-//     this.setState = this.setState.bind(this);
-//     this.step = []
-//     this.final = []
-//   };
-
-  
-
-//   componentDidMount(){
-//     // console.log(this.step)
-//   };
-
-//   state = {
-//     row: [0,1,2,3,4,5,6,7,8],
-//     col: [0,1,2,3,4,5,6,7,8],
-//     elements: Array(81).fill(0)
-//   }
-
-//   boxStyle = {
-//     width: "550px", 
-//     alignItems: "center", 
-//     border: "3px solid #000", 
-//     margin: "auto", 
-//     padding: "5px",
-//     justifyContent: "center"
-//   }
-
- 
-//   animateSolution(step){
-//     const arr = [...this.step][0]
-//     arr.forEach(sub => sub.forEach((swtch,i) => this.anime(swtch,i)));
-//   }
-
-//   fastSolution(){
-//     const arr = [...this.final][0]
-//     console.log(this.step[0])
-//     this.setState({
-//         row: [...this.state.row],
-//         col: [...this.state.col],
-//         elements: [...arr]
-//       })
-//     };
-
-//   anime(swtch, i){
-//     // console.log(this.state)
-//     (async () => {
-//       await tim(1) 
-//       this.changeState(this.setState, swtch);
-//     })()
-    
-//   }
- 
-//   changeState(setState, swtch) {
-//     // this.state.elements = [...this.state.elements.slice(0,swtch[0]), swtch[1], ...this.state.elements.slice(swtch[0]+1)]
-//     setState({
-//       row: [...this.state.row],
-//       col: [...this.state.col],
-//       elements: [...this.state.elements.slice(0,swtch[0]), swtch[1], ...this.state.elements.slice(swtch[0]+1)]
-//     })
-//   }
-
-//   render() {
-//     return (
-//     )
-//   }
-// }
-
-// const tim = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-
 export default function App() {
   const [elements, setElements] = useState(Array(81).fill(0))
+  const [steps, setSteps] = useState([])
+  const [init, setInit] = useState([])
+
   const initGrid = (arr) => {
     var newList = elements
     arr.forEach(step => {
@@ -88,29 +17,33 @@ export default function App() {
       newList[index] = val
       
     })
-    setElements(newList)
+    setElements(() => newList)
   }
-    const results = {"steps": [], "init": []} 
   useEffect(() => {
     fetch('/api/')
     .then(res => res.json())
     .then(data => {
-      console.log(data)
-      results.steps = data["steps"]; 
-      results.init = data["init"]; 
-      // this.final.push(data["final"]);
-      // this.setState({
-      // row: [...this.state.row],
-      // col: [...this.state.col],
-      // elements: data["init"]})
+      setSteps(() => data["steps"])
+      setInit(() => data["init"])
+      initGrid(data["init"])
     })
     .catch(e => console.log(e))
-    
-    initGrid(results.init)
-    console.log(results);
-
   },[]) 
-  
+  const results = {"steps": [], "init": []} 
+
+  const animate = (arr) => {
+    arr.forEach((step, i) => 
+    // var elems = 
+    // console.log(f, i)
+    setTimeout(()=>{
+      var newList = steps
+      var [index, val] = step
+      newList[index] = val
+      setElements((e) => [...e.slice(0, index), val, e.slice(index+1)])
+      console.log(index, val)
+
+    }, i*1))
+  }
 
   return (
       <div className="App">
@@ -135,7 +68,7 @@ export default function App() {
             marginTop: "15px",
             marginLeft: "15px"
         }}>Quick Solve</Button>
-        <Button onClick={()=>initGrid(results.init)} 
+        <Button onClick={()=>animate(steps)} 
             style={{
             background: "cadetblue",
             color: "white",
--- ui/src/components/Cell.js
+++ ui/src/components/Cell.js
@@ -5,7 +5,7 @@ import {Animate} from "react-simple-animate"
 
 export default function Cell(props) {
     const index = (props.col*9 + props.row);
-
+    const val = props.elements[index]
     // const [myState, setMystate] = useState({"init":true})
 
     // useEffect(() => {
@@ -13,25 +13,23 @@ export default function Cell(props) {
     // }, [myState])
 
     return (
-        <Animate play start={{ opacity: 0 }} end={{ opacity: 1 }} duration={1}>
             <Container style={boxStyle}>
                 <Row gutter={1}>
-                    <Col flex={12} style={getStyle(index)}> {props.elements[index]} </Col>
+                    <Col flex={12} style={getStyle(index, props.elements[index])}> {val>0? val: 0} </Col>
                 </Row>
             </Container>
-        </Animate>
     )
 }
 
 const subgroups = [["a", 0, 1, 2, 9, 10, 11, 18, 19, 20], ["b", 3, 4, 5, 12, 13, 14, 21, 22, 23], ["c", 6, 7, 8, 15, 16, 17, 24, 25, 26], ["d", 27, 28, 29, 36, 37, 38, 45, 46, 47], ["e", 30, 31, 32, 39, 40, 41, 48, 49, 50], ["f", 33, 34, 35, 42, 43, 44, 51, 52, 53], ["g", 54, 55, 56, 63, 64, 65, 72, 73, 74], ["h", 57, 58, 59, 66, 67, 68, 75, 76, 77], ["i", 60, 61, 62, 69, 70, 71, 78, 79, 80]]
 
-function getStyle(index) {
+function getStyle(index, val) {
     let box = subgroups.filter((li) => li.includes(index));
     box = box[0][0];
     const style = {
         background: "steelblue", 
         padding: "10px", 
-        color: "#fff",
+        color: val > 0? "#ff0000": "#fff",
         fontWeight: "bold" 
     }
     if(["a", "c", "e", "g", "i"].includes(box))
@@ -40,18 +38,11 @@ function getStyle(index) {
         return {
             background: "#222", 
             padding: "10px", 
-            color: "#ff0000",
+            color: val > 0? "#ff0000": "#fff",
             fontWeight: "bold"
         }
     }
 }
-
-// const preStyle = {
-//     background: "steelblue", 
-//     padding: "10px", 
-//     color: "#fff",
-//     fontWeight: "bold"
-// }
 
 const boxStyle = {
     width: "50px", 
